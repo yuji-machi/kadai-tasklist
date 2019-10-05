deb7910a99675bdb5ed42f164b5dab837d3e78af
yuji.machi <yuji.m46n@gmail.com>
2019-10-05 07:24:31 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+// CRUD
+// メッセージの個別詳細ページ表示
+Route::get('tasks/{id}', 'TasksController@show');
+// メッセージの新規登録を処理
+Route::post('tasks', 'TasksController@store');
+// メッセージの更新処理
+Route::put('tasks/{id}', 'TasksController@update');
+// メッセージを削除
+Route::delete('tasks/{id}', 'TasksController@destroy');
+
+// index: showの補助ページ
+Route::get('tasks', 'TasksController@index');
+// create: 新規作成用のフォームページ
+Route::get('tasks/create', 'TasksController@create');
+// edit: 更新用のフォームページ
+Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
