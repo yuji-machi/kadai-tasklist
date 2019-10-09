7103a852a8defa6e63fb8c2ac5bdabd09e3b30b3
yuji.machi <yuji.m46n@gmail.com>
2019-10-09 10:51:37 +0000

    file update

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
 
-@if (count($errors) > 0)
-        <ul class="alert alert-danger" role="alert">
-            @foreach ($errors->all() as $error)
-                <li class="ml-4">{{ $error }}</li>
-            @endforeach
-        </ul>
-@endif
-
 <h1>タスク新規追加ページ</h1>
 {!! Form::model($task, ['route' => 'tasks.store']) !!}
     <div class="row">
